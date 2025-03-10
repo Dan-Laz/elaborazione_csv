5ce3cbed9e4697703cad6d564b228e13be0bd64c
LAZZARONI.21130 <lazzaroni.daniel.studente@itispaleocpapa.it>
2025-03-10 13:13:21 +0100

    MONUMENTO

--- src/main/java/csv/Monumento.java
+++ src/main/java/csv/Monumento.java
@@ -12,6 +12,97 @@ public class Monumento {
     private double longitudine;
     private double latitudine;
 
+    public Monumento(String comune, String provincia, String regione, String nome, String tipo, String anno_inserimento, String data_ora_inserimento, int identificatore_OSM, double longitudine, double latitudine) {
+        this.comune = comune;
+        this.provincia = provincia;
+        this.regione = regione;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.anno_inserimento = anno_inserimento;
+        this.data_ora_inserimento = data_ora_inserimento;
+        this.identificatore_OSM = identificatore_OSM;
+        this.longitudine = longitudine;
+        this.latitudine = latitudine;
+    }
 
+    public String getComune() {
+        return comune;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public String getRegione() {
+        return regione;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getAnno_inserimento() {
+        return anno_inserimento;
+    }
+
+    public String getData_ora_inserimento() {
+        return data_ora_inserimento;
+    }
+
+    public int getIdentificatore_OSM() {
+        return identificatore_OSM;
+    }
+
+    public double getLongitudine() {
+        return longitudine;
+    }
+
+    public double getLatitudine() {
+        return latitudine;
+    }
+
+    public void setComune(String comune) {
+        this.comune = comune;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public void setRegione(String regione) {
+        this.regione = regione;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setAnno_inserimento(String anno_inserimento) {
+        this.anno_inserimento = anno_inserimento;
+    }
+
+    public void setData_ora_inserimento(String data_ora_inserimento) {
+        this.data_ora_inserimento = data_ora_inserimento;
+    }
+
+    public void setIdentificatore_OSM(int identificatore_OSM) {
+        this.identificatore_OSM = identificatore_OSM;
+    }
+
+    public void setLongitudine(double longitudine) {
+        this.longitudine = longitudine;
+    }
+
+    public void setLatitudine(double latitudine) {
+        this.latitudine = latitudine;
+    }
 }
 
