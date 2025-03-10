7069d3b6dbe10e3e0fcda0348d1a949b9f6d49e4
LAZZARONI.21130 <lazzaroni.daniel.studente@itispaleocpapa.it>
2025-03-10 13:39:50 +0100

    primo punto

---
+++ src/main/java/csv/Main.java
@@ -0,0 +1,8 @@
+package csv;
+
+public class Main {
+    public static void main(String[] args){
+        Monumenti m = new Monumenti();
+        m.aggiungiValori();
+    }
+}
--- src/main/java/csv/Monumenti.java
+++ src/main/java/csv/Monumenti.java
@@ -1,6 +1,23 @@
 package csv;
 
+import java.io.*;
+import java.security.Permission;
+
 public class Monumenti {
+
+    public void aggiungiValori(){
+        try(BufferedReader br = new BufferedReader(new FileReader("src/main/java/csv/lazzaroni.csv"));
+            PrintWriter pw = new PrintWriter(new FileWriter("src/main/java/csv/test.txt"))){
+            String line = "";
+            while ((line = br.readLine())!=null){
+                line = line.substring(0,line.length()-1);
+                pw.print(line+";"+(int)(Math.random()*10+10)+";"+"false\n");
+            }
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 
 
 }
